ca1747d85c922e2db8d29c27ac4366d3a647e342
PantaSreelakshmi22 <119397872+RaghavareddyVignesh@users.noreply.github.com>
2025-06-28 22:43:33 +0530

    Changed PLSQL Codes

--- Week_2_PLSQL/Exercise_1-Control_Structure/Scenario_1.sql
+++ Week_2_PLSQL/Exercise_1-Control_Structure/Scenario_1.sql
@@ -1,6 +1,17 @@
-SELECT 
-  C.Name, C.Age, L.LoanID, L.InterestRate
-FROM 
-  Customers C
-JOIN 
-  Loans L ON C.CustomerID = L.CustomerID;
+BEGIN
+    FOR c IN (
+        SELECT l.LoanID, l.InterestRate
+        FROM Loans l
+        JOIN Customers c ON l.CustomerID = c.CustomerID
+        WHERE MONTHS_BETWEEN(SYSDATE, c.DOB) / 12 > 60
+    ) LOOP
+        UPDATE Loans
+        SET InterestRate = InterestRate - 1
+        WHERE LoanID = c.LoanID;
+    END LOOP;
+    COMMIT;
+END;
+/
+
+
+SELECT * FROM Loans;
--- Week_2_PLSQL/Exercise_1-Control_Structure/Scenario_2.sql
+++ Week_2_PLSQL/Exercise_1-Control_Structure/Scenario_2.sql
@@ -1,2 +1,11 @@
-SELECT Name, Balance, IsVIP
-FROM Customers;
+BEGIN
+    FOR c IN (SELECT CustomerID FROM Customers WHERE Balance > 10000) LOOP
+        UPDATE Customers
+        SET IsVIP = 'Y'
+        WHERE CustomerID = c.CustomerID;
+    END LOOP;
+    COMMIT;
+END;
+/
+
+SELECT CustomerID, Name, Balance, IsVIP FROM Customers;
