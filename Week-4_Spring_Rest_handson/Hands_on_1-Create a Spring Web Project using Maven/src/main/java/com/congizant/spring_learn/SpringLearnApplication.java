1949513a5e1afb03048d1ccae4741173f447a79c
Vignesh Reddy <119397872+RaghavareddyVignesh@users.noreply.github.com>
2025-07-12 21:28:49 +0530

    Added week4 spring rest hands on 1

---
+++ Week-4_Spring_Rest_handson/Hands_on_1-Create a Spring Web Project using Maven/src/main/java/com/congizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,13 @@
+package com.congizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	public static void main(String[] args) {
+		System.out.println("✅ SpringLearnApplication is starting...");
+		SpringApplication.run(SpringLearnApplication.class, args);
+	}
+}
