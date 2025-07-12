1a9d1f040fcadfa9ba0ac9dd87cf9b864d4d583c
Vignesh Reddy <119397872+RaghavareddyVignesh@users.noreply.github.com>
2025-07-12 22:00:01 +0530

    added week 4 hands on files

---
+++ Week-4_Spring_Rest_handson/2.Spring_Rest_Hands_on/Hello_World_RESTful_Web_Service /src/test/java/com/congizant/spring_learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congizant.spring_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
